52c0ba58f33bfec0f5c988784df78583d1d76d28
Vincent Landgraf <vilandgr+sqlite@googlemail.com>
2015-03-09 08:33:28 +0100

    add libsqlite3 to travis build and change version to 1.0.0

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -1,6 +1,9 @@
 module SQLite3
-  VERSION = '1.3.9'
-  
+  MAJOR = 1
+  MINOR = 0
+  PATCH = 0
+  VERSION = [MAJOR, MINOR, PATCH].join('.')
+
   def self.libversion
     Driver.sqlite3_libversion().to_s
   end
